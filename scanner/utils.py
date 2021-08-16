c5aed59ac9c199ed9460f3a20c4311719cd097fa
dmytroshurko <dmytro.shurko@protonmail.com>
2021-08-16 22:48:27 +0300

    update utility functions

--- scanner/utils.py
+++ scanner/utils.py
@@ -2,38 +2,35 @@
 # Utility functions.
 
 import json
-from typing import Dict, List
+from typing import Dict
 
 
-def load_list(filepath: str) -> List[Dict]:
-    """Load a list of dictionaries from a JSON's filepath.
+def load_dict(filepath: str) -> Dict:
+    """Load a dictionary from a JSON's filepath.
 
     Args:
         filepath (str): JSON's filepath.
 
     Returns:
-        A list with the data loaded.
+        A dictionary with the data loaded.
     """
     with open(filepath) as fp:
-        lst = json.load(fp)
-        return lst
+        d = json.load(fp)
+    return d
 
 
-def save_list(
-    lst: List[Dict],
-    filepath: str,
-    ensure_ascii: bool = False,
-    sortkeys: bool = False,
+def save_dict(
+    d: Dict, filepath: str, ensure_ascii: bool = False, sortkeys: bool = False
 ) -> None:
-    """Save a list of dictionaries to a specific location.
+    """Save a dictionary to a specific location.
 
     Args:
-        lst (List[Dict]): A list of dictionaries to save.
-        filepath (str): A location to save the list to as a JSON file.
+        d (Dict): A dictionary to save.
+        filepath (str): A location to save the dictionary to as a JSON file.
         ensure_ascii (bool, optional): Ensure the output is valid ascii characters. Defaults to False.
         sortkeys (bool, optional): Sort keys in dict alphabetically. Defaults to False.
     """
     with open(filepath, "w") as fp:
         json.dump(
-            lst, fp=fp, ensure_ascii=ensure_ascii, indent=2, sort_keys=sortkeys
+            d, fp=fp, ensure_ascii=ensure_ascii, indent=2, sort_keys=sortkeys
         )
