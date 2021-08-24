15e1c028f2bba84423d133053dd3e362b7e0f383
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-08-24 13:41:19 +0300

    create a first vue App

--- card-editor/assets/js/app.js
+++ card-editor/assets/js/app.js
@@ -1,1 +1,13 @@
+// impotr card JSON file from folder
+import json from "../../B2-1.json" assert { type: "json" }
 
+// start vue app
+const App = {
+  data() {
+    return {
+
+    }
+  },
+}
+
+Vue.createApp(App).mount("#container")
